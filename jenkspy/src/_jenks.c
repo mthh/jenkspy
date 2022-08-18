886c5670cf2b7a57718a98de2d6bb9f2b76ffc8f
Matthieu Viry <matthieu.viry@univ-grenoble-alpes.fr>
2022-08-18 07:58:25 +0200

    Revert extra array allocation introduced in 30abfe4

--- jenkspy/src/_jenks.c
+++ jenkspy/src/_jenks.c
@@ -16,37 +16,38 @@ JenksBreakValues(double *values, unsigned int nb_class,
     int lower_class_limit, i4, k;
     unsigned int i=0, j=0, l=0, m=0;
     double variance, val, sum, sum_squares, w, temp_val;
-    int **lower_class_limits = (int **)malloc((length_array + 1) * sizeof(int*)),
+    int **lower_class_limits = (int **)malloc(length_array * sizeof(int*)),
         *row_int = NULL;
-    double **variance_combinations = (double **)malloc((length_array + 1) * sizeof(double*)),
+    double **variance_combinations = (double **)malloc(length_array * sizeof(double*)),
         *row_double = NULL;
 
     // Sort the target array
     qsort(values, length_array, sizeof(double), compare_doubles);
 
     // Initialise the lower_class_limits and variance_combinations arrays
-    for (i = 0; i < length_array + 1; i++) {
-        row_int = (int *)malloc((nb_class + 1) * sizeof(int));
-        memset(row_int, 0, (nb_class + 1) * sizeof(int));
+    for (i = 0; i < length_array; i++) {
+        row_int = (int *)malloc(nb_class * sizeof(int));
+        memset(row_int, 1, nb_class * sizeof(int));
         lower_class_limits[i] = row_int;
 	}
 
-    for (i = 0; i < length_array + 1; i++) {
-        row_double = (double *)malloc((nb_class + 1) * sizeof(double));
-        memset(row_double, 0.0, (nb_class + 1) * sizeof(double));
+    for (i = 0; i < length_array; i++) {
+        row_double = (double *)malloc(nb_class * sizeof(double));
+        memset(row_double, 0.0, nb_class * sizeof(double));
         variance_combinations[i] = row_double;
 	}
 
-    for (i = 1; i < nb_class + 1; i++) {
-        lower_class_limits[0][i] = 1;
-        lower_class_limits[1][i] = 1;
-        for (j = 2; j < length_array + 1; j++) { variance_combinations[j][i] = DBL_MAX; }
+    for (i = 1; i < length_array; i++) {
+        for (j = 0; j < nb_class; j++) {
+            variance_combinations[i][j] = DBL_MAX;
+        }
     }
 
     variance = 0;
     for (l = 2; l < length_array + 1; l++) {
         sum = sum_squares = w = 0.0;
         for (m = 1; m < l + 1; m++) {
+
             lower_class_limit = l - m + 1;
 
             val = values[lower_class_limit - 1];
@@ -59,16 +60,16 @@ JenksBreakValues(double *values, unsigned int nb_class,
 
             if(i4 != 0) {
                 for (j = 2; j < nb_class + 1; j++) {
-                    temp_val = (variance + variance_combinations[i4][j - 1]);
-                    if (fabs(variance_combinations[l][j] - temp_val) < DBL_EPSILON || variance_combinations[l][j] > temp_val) {
-                        lower_class_limits[l][j] = lower_class_limit;
-                        variance_combinations[l][j] = temp_val;
+                    temp_val = (variance + variance_combinations[i4 - 1][j - 2]);
+                    if (fabs(variance_combinations[l - 1][j - 1] - temp_val) < DBL_EPSILON || variance_combinations[l - 1][j - 1] > temp_val) {
+                        lower_class_limits[l - 1][j - 1] = lower_class_limit;
+                        variance_combinations[l - 1][j - 1] = temp_val;
                     }
                 }
             }
         }
-        lower_class_limits[l][1] = 1;
-        variance_combinations[l][1] = variance;
+        lower_class_limits[l - 1][0] = 1;
+        variance_combinations[l - 1][0] = variance;
     }
 
     // Prepare the class limits
@@ -79,12 +80,12 @@ JenksBreakValues(double *values, unsigned int nb_class,
     breaks[nb_class] = values[(int)length_array - 1];
 
     for (j = nb_class; j > 1; j--) {
-        breaks[j - 1] = values[lower_class_limits[k][j] - 2];
-        k = lower_class_limits[k][j] - 1;
+        breaks[j - 1] = values[lower_class_limits[k - 1][j - 1] - 2];
+        k = lower_class_limits[k - 1][j - 1] - 1;
     }
 
     // Free lower_class_limits and variance_combinations arrays
-    for (i = 0; i < length_array + 1; i++) {
+    for (i = 0; i < length_array; i++) {
         free(lower_class_limits[i]);
         free(variance_combinations[i]);
     }
