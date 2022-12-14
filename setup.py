20da64dc377d185e1604c2324bd7e75d7b6e1cdd
Matthieu Viry <matthieu.viry@univ-grenoble-alpes.fr>
2022-12-14 10:17:33 +0100

    Add 'typed' pypi classifiers in setup.py

--- setup.py
+++ setup.py
@@ -59,5 +59,6 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering",
+        "Typing :: Typed",
     ],
 )
