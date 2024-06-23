4887b61538fd802c7f1b7dd67a7eca5b23eef3a2
Raashid Salih <61780547+raashidsalih@users.noreply.github.com>
2024-06-23 13:19:48 +0530

    Update __init__.py with added function

--- jenkspy/__init__.py
+++ jenkspy/__init__.py
@@ -5,6 +5,7 @@ __version__ = "0.4.1"
 from .core import jenks_breaks
 from .core import _jenks_matrices
 from .core import JenksNaturalBreaks
+from .core import elbow_chart
 
 
-__all__ = ['jenks_breaks', '_jenks_matrices', 'JenksNaturalBreaks']
+__all__ = ['jenks_breaks', '_jenks_matrices', 'JenksNaturalBreaks', 'elbow_chart']
